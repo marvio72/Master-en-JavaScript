7005ed79e0357370f2b55b2828f435e9eb4e9fce
marvio72 <mruvalcaba@oversistemas.com>
2021-09-22 16:00:32 -0500

    Selectores de Clases

--- aprendiendo-jquery/js/01-selectores.js
+++ aprendiendo-jquery/js/01-selectores.js
@@ -5,4 +5,12 @@ $(document).ready(function () {
   $('#amarillo').css('background', 'yellow').css('color', 'green');
 
   $('#verde').css('background', 'green').css('color', 'yellow');
+
+  // Selector de clases
+  let mi_clase = $('.zebra').css('padding', '5px');
+
+  $('.sin_borde').click(function () {
+    console.log('Click dado!!');
+    $(this).addClass('zebra');
+  });
 });
