ed809c2f886b4d37533fea4aad023701c2db5762
marvio72 <mruvalcaba@oversistemas.com>
2021-09-22 14:41:00 -0500

    Selectores ID

---
+++ aprendiendo-jquery/js/01-selectores.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  // Selector de ID
+  $('#rojo').css('background', 'red').css('color', 'white');
+
+  $('#amarillo').css('background', 'yellow').css('color', 'green');
+
+  $('#verde').css('background', 'green').css('color', 'yellow');
+});
