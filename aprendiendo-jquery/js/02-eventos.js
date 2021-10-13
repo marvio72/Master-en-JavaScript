16eab2c791c65ff905fd96f9e4a2c832c7854361
marvio72 <mruvalcaba@oversistemas.com>
2021-10-13 17:32:34 -0500

    Hover

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
   // Eventos MouseOver y MouseOut
   let caja = $('#caja');
 
-  caja.mouseover(function () {
-    $(this).css('background', 'red');
-  });
+  // caja.mouseover(function () {
+  //   $(this).css('background', 'red');
+  // });
 
-  caja.mouseout(function () {
+  // caja.mouseout(function () {
+  //   $(this).css('background', 'green');
+  // });
+
+  function cambiaRojo() {
+    $(this).css('background', 'red');
+  }
+
+  function cambiaVerde() {
     $(this).css('background', 'green');
-  });
+  }
+
+  caja.hover(cambiaRojo, cambiaVerde);
 });
