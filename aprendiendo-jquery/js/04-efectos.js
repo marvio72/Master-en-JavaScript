20df4eeebc31668d73e5836de1a865e2e50dd0df
marvio72 <mruvalcaba@oversistemas.com>
2021-10-23 19:21:23 -0500

    Efectos personalizados

--- aprendiendo-jquery/js/04-efectos.js
+++ aprendiendo-jquery/js/04-efectos.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  let caja = $('#caja');
   $('#mostrar').hide();
 
   $('#mostrar').click(function () {
@@ -6,17 +7,26 @@ $(document).ready(function () {
     $('#ocultar').show();
     // $('#caja').show('slow');
     // $('#caja').fadeIn('slow');
-    $('#caja').fadeTo('slow', 1);
+    caja.fadeTo('slow', 1);
   });
 
   $('#ocultar').click(function () {
     $(this).hide();
     $('#mostrar').show();
     // $('#caja').hide('slow');
-    $('#caja').fadeTo('slow', 0);
+    caja.fadeTo('slow', 0);
   });
 
   $('#todoenuno').click(function () {
-    $('#caja').slideToggle('fast');
+    caja.slideToggle('fast');
+  });
+
+  $('#animar').click(function () {
+    caja
+      .animate({ marginLeft: '500px', fontSize: '40px', height: '110px' }, 'slow')
+      .animate({ borderRadius: '900px', marginTop: '200px' }, 'slow')
+      .animate({ borderRadius: '0px', marginLeft: '0px' }, 'slow')
+      .animate({ borderRadius: '100px', marginTop: '0px' }, 'slow')
+      .animate({ marginLeft: '500px', fontSize: '40px', height: '110px' }, 'slow');
   });
 });
