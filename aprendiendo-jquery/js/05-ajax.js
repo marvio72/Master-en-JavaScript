36df312f3cddf2053c3a096588080f433f530fd1
marvio72 <mruvalcaba@oversistemas.com>
2021-10-24 08:40:04 -0500

    Método Get - AJAX

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -4,5 +4,15 @@ $(document).ready(function () {
     Incrustar dados por ejemplo en un div
     */
 
-  $('#datos').load('https://reqres.in/'); //Nos incrusta en un div toda la pagina solicitada.
+  //   $('#datos').load('https://reqres.in/'); //Nos incrusta en un div toda la pagina solicitada.
+
+  /*
+  Metodo Get y Post
+  */
+
+  $.get('https://reqres.in/api/users', { page: 1 }, function (response) {
+    response.data.forEach((element, index) => {
+      $('#datos').append('<p>' + element.first_name + ' ' + element.last_name + '</p>');
+    });
+  });
 });
