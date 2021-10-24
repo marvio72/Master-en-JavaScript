6e9d01e23cc5329983f7ceb46662bf04b7813a33
marvio72 <mruvalcaba@oversistemas.com>
2021-10-23 19:50:16 -0500

    Load Ajax

---
+++ aprendiendo-jquery/js/05-ajax.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  /*
+    Metodo Load
+    Incrustar dados por ejemplo en un div
+    */
+
+  $('#datos').load('https://reqres.in/'); //Nos incrusta en un div toda la pagina solicitada.
+});
