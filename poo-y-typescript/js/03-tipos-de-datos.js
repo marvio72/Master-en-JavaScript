d7a04c3a02694c5d72468927db38f931a705070e
marvio72 <mruvalcaba@oversistemas.com>
2021-11-06 21:21:41 -0600

    Tipos de datos personalizados

--- poo-y-typescript/js/03-tipos-de-datos.js
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -1,3 +1,5 @@
+var caracteres = 'MarcoRuvalcaba';
+caracteres = 17;
 // multiple tipo de datos, con el simbolo de tubo podemos agregar varios tipos de datos.
 var cadena = 'Marco Ruvalcaba';
 cadena = 15;
@@ -11,4 +13,4 @@ cualquiera = 77;
 // Arrays
 var lenguajes = ['PHP', 'JS', 'CSS'];
 var years = [12, 13, 14];
-console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years, caracteres);
--- poo-y-typescript/js/03-tipos-de-datos.ts
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -1,3 +1,10 @@
+// Tipo de dato personalizado
+type alfanumerico = string | number;
+
+let caracteres: alfanumerico = 'MarcoRuvalcaba';
+
+caracteres = 17;
+
 // multiple tipo de datos, con el simbolo de tubo podemos agregar varios tipos de datos.
 let cadena: string | number = 'Marco Ruvalcaba';
 
@@ -18,4 +25,4 @@ let lenguajes: Array<string> = ['PHP', 'JS', 'CSS'];
 
 let years: number[] = [12, 13, 14];
 
-console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years, caracteres);
