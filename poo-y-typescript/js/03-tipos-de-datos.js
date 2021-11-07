a500c00d6b8d1fb3fd2ae7e0cb1635fe159485f8
marvio72 <mruvalcaba@oversistemas.com>
2021-11-06 21:14:30 -0600

    Variables y tipos de datos en TS

---
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -0,0 +1,13 @@
+// string
+var cadena = 'Marco Ruvalcaba';
+// number
+var numero = 12;
+// booleano
+var verdadero_falso = true;
+// Any
+var cualquiera = 'hola';
+cualquiera = 77;
+// Arrays
+var lenguajes = ['PHP', 'JS', 'CSS'];
+var years = [12, 13, 14];
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
---
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -0,0 +1,19 @@
+// string
+let cadena: string = 'Marco Ruvalcaba';
+
+// number
+let numero: number = 12;
+
+// booleano
+let verdadero_falso: boolean = true;
+
+// Any
+let cualquiera: any = 'hola';
+cualquiera = 77;
+
+// Arrays
+let lenguajes: Array<string> = ['PHP', 'JS', 'CSS'];
+
+let years: number[] = [12, 13, 14];
+
+console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
