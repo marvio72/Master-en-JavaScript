1c5732a3e66391ed9e8153d0c56f0c92b58d2fcd
marvio72 <mruvalcaba@oversistemas.com>
2021-11-09 12:00:44 -0600

    Repaso Métodos

--- poo-y-typescript/js/coche.js
+++ poo-y-typescript/js/coche.js
@@ -1,5 +1,6 @@
 var Coche = /** @class */ (function () {
     function Coche() {
+        this.velocidad = 0;
     }
     Coche.prototype.getColor = function () {
         return this.color;
@@ -7,14 +8,38 @@ var Coche = /** @class */ (function () {
     Coche.prototype.setColor = function (color) {
         this.color = color;
     };
+    Coche.prototype.getVelocidad = function () {
+        return this.velocidad;
+    };
+    Coche.prototype.setVelocidad = function (velocidad) {
+        this.velocidad = velocidad;
+    };
+    // Metodos
+    Coche.prototype.acelerar = function () {
+        this.velocidad++;
+    };
+    Coche.prototype.frenar = function () {
+        this.velocidad--;
+    };
     return Coche;
 }());
 var coche = new Coche();
 var coche2 = new Coche();
 var coche3 = new Coche();
 coche.setColor('Negro');
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.frenar();
+coche.frenar();
+coche.frenar();
 coche2.setColor('Azul');
 coche3.setColor('Rojo');
-console.log("El color del codhe es: " + coche.getColor());
-console.log("El color del codhe2 es: " + coche2.getColor());
-console.log("El color del codhe3 es: " + coche3.getColor());
+console.log("El color del codhe es: " + coche.getColor() + " y tiene una velocidad de: " + coche.getVelocidad() + " K/h");
+// console.log(`El color del codhe2 es: ${coche2.getColor()}`);
+// console.log(`El color del codhe3 es: ${coche3.getColor()}`);
--- poo-y-typescript/js/coche.ts
+++ poo-y-typescript/js/coche.ts
@@ -1,7 +1,7 @@
 class Coche {
   public color: string;
   public modelo: string;
-  public velocidad: number;
+  public velocidad: number = 0;
 
   public getColor() {
     return this.color;
@@ -10,15 +10,46 @@ class Coche {
   public setColor(color) {
     this.color = color;
   }
+
+  public getVelocidad(): number {
+    return this.velocidad;
+  }
+
+  public setVelocidad(velocidad) {
+    this.velocidad = velocidad;
+  }
+
+  // Metodos
+  public acelerar() {
+    this.velocidad++;
+  }
+
+  public frenar() {
+    this.velocidad--;
+  }
 }
 
 let coche = new Coche();
 let coche2 = new Coche();
 let coche3 = new Coche();
 coche.setColor('Negro');
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+coche.frenar();
+coche.frenar();
+coche.frenar();
+
 coche2.setColor('Azul');
 coche3.setColor('Rojo');
 
-console.log(`El color del codhe es: ${coche.getColor()}`);
-console.log(`El color del codhe2 es: ${coche2.getColor()}`);
-console.log(`El color del codhe3 es: ${coche3.getColor()}`);
+console.log(
+  `El color del codhe es: ${coche.getColor()} y tiene una velocidad de: ${coche.getVelocidad()} K/h`
+);
+// console.log(`El color del codhe2 es: ${coche2.getColor()}`);
+// console.log(`El color del codhe3 es: ${coche3.getColor()}`);
