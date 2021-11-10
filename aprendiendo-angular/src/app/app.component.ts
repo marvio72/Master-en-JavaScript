72be949cd96d0485eb5ba450d1dff993bbdbdf05
marvio72 <mruvalcaba@oversistemas.com>
2021-11-10 15:40:38 -0600

    Hooks / Eventos ciclo de vida

--- aprendiendo-angular/src/app/app.component.ts
+++ aprendiendo-angular/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Master de JavaScript y Angular';
+  public title = 'Master de JavaScript y Angular';
+  public mostrar_videojuegos: boolean = true;
+
+  ocultarVideojuegos(value: boolean) {
+    this.mostrar_videojuegos = value;
+  }
 }
--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'videojuego',
   templateUrl: './videojuego.component.html',
 })
-export class VideojuegoComponent {
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy {
   public titulo: string;
   public listado: string;
   constructor() {
     this.titulo = 'Componente de Videojuegos';
     this.listado = 'Listado de los juegos más populares';
     console.log('Se ha cargado el componente: videojuego.component.ts');
+  }
+  ngOnInit() {
+    console.log('OnInit iniciado!!');
+  }
+
+  ngDoCheck() {
+    console.log('DoCheck Ejecutado!!!');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy Ejecutado!!!!');
+  }
+
+  cambiarTitulo() {
+    this.titulo = 'Nuevo titulo del componente';
   }
 }
