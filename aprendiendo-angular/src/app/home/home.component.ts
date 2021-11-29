015a349842845686d4ce011439d917828b2fca0e
marvio72 <mruvalcaba@oversistemas.com>
2021-11-29 15:04:06 -0600

    ngTemplate - If y else en las vistas

--- aprendiendo-angular/src/app/home/home.component.ts
+++ aprendiendo-angular/src/app/home/home.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public identificado: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.identificado = false;
   }
 
+  ngOnInit(): void {}
+
+  setIdentificado() {
+    this.identificado = true;
+  }
+
+  unsetIdentificado() {
+    this.identificado = false;
+  }
 }
