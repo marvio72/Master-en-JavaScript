cb49650cb5fde5c652efd4abb5b9bd48f504b786
marvio72 <mruvalcaba@oversistemas.com>
2021-12-27 02:06:12 -0600

    Diseño pagina About

--- proyecto-angular/src/app/components/about/about.component.ts
+++ proyecto-angular/src/app/components/about/about.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  public title: String;
+  public subtitle: String;
+  public web: String;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.title = 'Marco Rafael Ruvalcaba Espinoza';
+    this.subtitle = 'Ing. en Sistemas y Desarrollador Web';
+    this.web = 'mruvalcaba.com';
   }
 
+  ngOnInit(): void {}
 }
